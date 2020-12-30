511e87e873970dcdc6aa81878351d0982c8e0108
jasonflorentino <jasonmflorentino@gmail.com>
2020-12-30 13:35:41 -0500

    ✏️ More detailed status updates in terminal.

--- update.py
+++ update.py
@@ -12,35 +12,38 @@ DATA_URL = "https://drive.google.com/file/d/11KF1DuN5tntugNc10ogQDzFnW05ruzLH/vi
 XPATH = "/html/body/div[3]/div[3]/div/div[3]/div[2]/div[2]/div[3]"
 FILE_NAME = "CityofToronto_COVID-19_Daily_Public_Reporting"
 FILE_EXTENSION = ".xlsx"
-ACTIVE_ROW = 297
+ACTIVE_ROW = 298
 ACTIVE_ROW_LINE_NO = 15
-PREVIOUS_DATE = '2020-12-27'
+PREVIOUS_DATE = '2020-12-28'
 PREVIOUS_DATE_LINE_NO = 17
+INDENT = "  "
 
 def wait(seconds):
-    print(f"Waiting {seconds} seconds...")
+    print(f"{INDENT}Waiting {seconds} seconds...")
     sleep(seconds)
 
 # Download Excel spreadsheet owned by City of Toronto using Selenium
 while True:
-    print("Opening browser...")
+    print("Getting data file...")
+    print(f"{INDENT}Opening browser...")
     browser = webdriver.Chrome(DRIVER_LOCATION)
     browser.get(DATA_URL)
     wait(4)
 
     button = browser.find_element_by_xpath(XPATH)
-    print(f"Found <{button.tag_name}> element.")
+    print(f"{INDENT}Found <{button.tag_name}> element.")
 
     button.click()
-    print(f"Clicked <{button.tag_name}> element.")
+    print(f"{INDENT}Clicked <{button.tag_name}> element.")
     wait(7)
 
-    print("Quitting browser...")
+    print(f"{INDENT}Quitting browser...")
     browser.quit()
     wait(4)
 
     # Get data from downloaded Excel spreadsheet
-    print("Opening downloaded file...")
+    print("Retrieving data from file...")
+    print(f"{INDENT}Opening downloaded file...")
     wb = None
     workingPath = ''
     fileAppendNum = 0
@@ -50,43 +53,43 @@ while True:
         if fileAppendNum == 0:
             try:
                 fileName = FILE_NAME + FILE_EXTENSION
-                print(f"Looking for {fileName}...")
+                print(f"{INDENT}Looking for {fileName}...")
                 wb = openpyxl.load_workbook(config.DOWNLOAD_FOLDER + fileName)
                 workingPath = config.DOWNLOAD_FOLDER + fileName
-                print(f"Opened {fileName}.")
+                print(f"{INDENT}Opened {fileName}.")
                 break
             except:
-                print(f"Could not find: {fileName}.\nTrying next name option...")
+                print(f"{INDENT}Could not find: {fileName}.\n{INDENT}Trying next name option...")
                 fileAppendNum += 1
         else:
             try:
                 downloadAppend = f" ({fileAppendNum})"
                 fileName = FILE_NAME + downloadAppend + FILE_EXTENSION
-                print(f"Looking for {fileName}...")
+                print(f"{INDENT}Looking for {fileName}...")
                 wb = openpyxl.load_workbook(config.DOWNLOAD_FOLDER + fileName)
                 workingPath = config.DOWNLOAD_FOLDER + fileName
-                print(f"Opened {fileName}.")
+                print(f"{INDENT}Opened {fileName}.")
                 break
             except:
-                print(f"Could not find: {fileName}.\nTrying next name option...")
+                print(f"{INDENT}Could not find: {fileName}.\n{INDENT}Trying next name option...")
                 fileAppendNum += 1
         if fileAppendNum == 4:
-            print(f"ERROR: Could not find file after {fileAppendNum} tries.")
+            print(f"{INDENT}ERROR: Could not find file after {fileAppendNum} tries.")
             break
                 
-    print("Retrieving date...")
+    print(f"{INDENT}Retrieving date...")
     dateSheet = wb['Cases by Reported Date']
     DATE = dateSheet['A2'].value
     DATE = str(DATE).split(" ")[0]
 
     # Halt if we've seen this data before
     if DATE == PREVIOUS_DATE:
-        print(f"Stopping execution: Spreadsheet data has already been read. (Already read data from: {DATE})")
-        print("Deleting downloaded file...")
+        print(f"{INDENT}Stopping execution: Spreadsheet data has already been read. (Already read data from: {DATE})")
+        print(f"{INDENT}Deleting downloaded file...")
         os.remove(workingPath)
         break
 
-    print("Retrieving data...")
+    print(f"{INDENT}Retrieving COVID data...")
     sheet = wb['Daily Status']
     TOTAL_CASE_COUNT = sheet['B2'].value
     RECOVERED = sheet['B5'].value
@@ -95,30 +98,34 @@ while True:
     CURRENTLY_ICU = sheet['B9'].value
 
     LATEST_DATA = [DATE, TOTAL_CASE_COUNT, RECOVERED, FATAL, CURRENTLY_HOSP, CURRENTLY_ICU]
-    print(f"Success! New data retrieved: {LATEST_DATA}")
+    print(f"{INDENT}Success! New data retrieved: {LATEST_DATA}")
 
     # Delete file once we have the data
-    print("Deleting downloaded file...")
+    print(f"{INDENT}Deleting downloaded file...")
     os.remove(workingPath)
 
     # Update Google Sheet
-    print("Accessing Google sheet...")
+    print("Updating Google Sheet with new data...")
+    print(f"{INDENT}Accessing Google sheet...")
     doc = ezsheets.Spreadsheet(config.GOOGLE_SHEET_ID)
     sheet = doc[0]
-    print(f"Updating next row... (Row: {ACTIVE_ROW})")
+    print(f"{INDENT}Updating row {ACTIVE_ROW}...")
     sheet.updateRow(ACTIVE_ROW, LATEST_DATA)
 
     # Update counters in this script for next time
-    print("Reading update.py...")
+    print("Updating source script...")
+    print(f"{INDENT}Reading update.py...")
     content = []
     with open(__file__,"r") as f:
         for line in f:
             content.append(line)
 
-    print("Writing update.py...")
+    print("  Writing update.py...")
     with open(__file__,"w") as f:
         nextRow = ACTIVE_ROW + 1
+        print(f"{INDENT}Writing: ACTIVE_ROW = {nextRow}")
         content[ACTIVE_ROW_LINE_NO - 1] = f"ACTIVE_ROW = {nextRow}\n"
+        print(f"{INDENT}Writing: PREVIOUS_DATE = '{DATE}'")
         content[PREVIOUS_DATE_LINE_NO - 1] = f"PREVIOUS_DATE = '{DATE}'\n"
         for i in range(len(content)):
             f.write(content[i])
